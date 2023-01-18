711edd1b2544b5a8900ff2c0d3f6906f33b7bca3
Ondra <ondra.baudisch@centrum.cz>
2023-01-18 09:56:28 +0100

    #

--- learning.py
+++ learning.py
@@ -20,7 +20,7 @@ s_size = 16384 * (24 // 2)
 steps_per_epoch = 10
 steps = 100
 noise_ratio = 0.7
-batch_size=5
+batch_size=7
 
 # model = wave_u_net(num_initial_filters = 12, num_layers = 6, kernel_size = 10, input_size = s_size, output_type = "single")
 
