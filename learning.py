d00e2948182581bf068f743c562ad0be8d2099bd
Ondra <ondra.baudisch@centrum.cz>
2023-01-18 09:53:01 +0100

    #

--- learning.py
+++ learning.py
@@ -20,6 +20,7 @@ s_size = 16384 * (24 // 2)
 steps_per_epoch = 10
 steps = 100
 noise_ratio = 0.7
+batch_size=10
 
 # model = wave_u_net(num_initial_filters = 12, num_layers = 6, kernel_size = 10, input_size = s_size, output_type = "single")
 
@@ -48,9 +49,10 @@ model.compile(loss="MSE", optimizer=opt)
 
 model.summary()
 
-dataset = Dataset([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], s_size=s_size, steps=steps, batch_size=1, noise_ratio=noise_ratio)
+dataset = Dataset([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], s_size=s_size, steps=steps, batch_size=batch_size, noise_ratio=noise_ratio)
 
 epochs = len(dataset) // steps_per_epoch
+print(f"data: {(len(dataset) * batch_size):_}")
 
 history = model.fit(dataset, epochs=epochs, steps_per_epoch=steps_per_epoch, initial_epoch=initial_epoch, shuffle=False, callbacks=[CustomCallback(chackpoint=True), ModelCheckpoint(filepath='model.h5', save_best_only=False, save_weights_only=True, save_freq='epoch')])
 
