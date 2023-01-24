575aee708b6378e1755e4c40cb273fd9b24a05d7
Ondra <ondra.baudisch@centrum.cz>
2023-01-24 16:42:09 +0100

    #

--- wave_u_net.py
+++ wave_u_net.py
@@ -108,8 +108,8 @@ class AttentionGate:
 
     x = tf.keras.layers.add([x, g])
     x = tf.keras.layers.Activation("relu")(x)
-    # x = tf.keras.layers.Conv1D(filters=1, kernel_size=1, strides=1, padding="same")(x)
-    # x = tf.keras.layers.Activation("sigmoid")(x)
+    x = tf.keras.layers.Conv1D(filters=1, kernel_size=1, strides=1, padding="same")(x)
+    x = tf.keras.layers.Activation("sigmoid")(x)
     # x = tf.keras.layers.UpSampling1D(size=inp.shape[1] // x.shape[1])(x)
 
     # y = tf.keras.layers.Multiply()([x, inp])
