d37c84f673da54f169a607352ac0450e897930da
Ondra <ondra.baudisch@centrum.cz>
2023-02-23 14:05:10 +0100

    #

--- testing.py
+++ testing.py
@@ -25,7 +25,7 @@ steps = 25
 
 # model = wave_u_net(num_initial_filters = 24, num_layers = 12, kernel_size = 15, input_size = s_size, output_type = "single", attention = "Gate", attention_res = False, dropout = "False", dropout_rate = 0.2)
 # model = wave_u_net(num_initial_filters = 32, num_layers = 16, kernel_size = 30, input_size = s_size, output_type = "single", attention = "Gate", attention_res = False, dropout = "False", dropout_rate = 0.2, sub=False, side_chanel=side, side_chanel_cycles=cycles)
-model = wave_u_net(num_initial_filters = 32, num_layers = 16, kernel_size = 50, input_size = s_size, output_type = "single", attention = "Gate", attention_res = False, dropout = False, dropout_rate = 0.2, sub=True, side_chanel=False, side_chanel_cycles=10)
+model = wave_u_net(num_initial_filters = 32, num_layers = 16, kernel_size = 50, input_size = s_size, output_type = "single", attention = "Gate", attention_res = False, dropout = False, dropout_rate = 0.2, sub=False, side_chanel=side, side_chanel_cycles=cycles)
 
 model.load_weights("model.h5")
 
