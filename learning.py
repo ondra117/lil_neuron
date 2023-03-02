589787cceec1210bd4d6c3774e2645e3c3744be3
Ondra <ondra.baudisch@centrum.cz>
2023-03-02 23:05:23 +0100

    #

--- learning.py
+++ learning.py
@@ -59,7 +59,7 @@ c2 = ModelCheckpoint(filepath='model.h5', save_best_only=False, save_weights_onl
 if side:
     dataset = DatasetSide(list(range(90)), s_size=s_size, steps=steps, batch_size=batch_size, noise_ratio=noise_ratio, orig=True, info=True, side_cysles=cycles)
 else:
-    dataset = Dataset(list(range(400, 500)), s_size=s_size, steps=steps, batch_size=batch_size, noise_ratio=noise_ratio, orig=True, info=True)
+    dataset = Dataset(list(range(500, 600)), s_size=s_size, steps=steps, batch_size=batch_size, noise_ratio=noise_ratio, orig=True, info=True)
 
 epochs = len(dataset) // steps_per_epoch
 print(f"data: {(len(dataset) * batch_size):_}")
