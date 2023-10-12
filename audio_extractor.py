c695a1301227dfabec9571278dde66c44ac8cfba
ondra117 <ondrabaudisch@gmail.com>
2023-10-12 09:01:28 +0200

    #

--- audio_extractor.py
+++ audio_extractor.py
@@ -1,100 +1,44 @@
-import youtube_dl
+from pytube import YouTube
+from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
 import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 from copy import deepcopy
 import os
 from time import sleep
+import ffmpeg
 
-
-
+output_folder = "music/"
 
 class AudioExtractor:
     def __init__(self):
         ...
 
-    def extract(self, url):
-        while True:
-            try:
-                video_info = youtube_dl.YoutubeDL().extract_info(url = url,download=False)
-            except youtube_dl.utils.DownloadError as e:
-                print(f"Error: {e}")
-                if "Sign in to confirm your age" in str(e):
-                    return False
-                print("Try again:")
-                continue
+    def extract(self, url, n):
+        filename = f"{n}.".zfill(6)
 
-            with open("music.json") as f:
-                data = json.load(f)
+        yt = YouTube(url)
 
-            filename = f"music/{data['n_songs']}.%(ext)s"
+        audio_stream = yt.streams.filter(only_audio=True).first()
 
-            song = {
-                "title":video_info["title"],
-                "lyrics":"",
-                "url":url
-            }
+        audio_stream.download(output_path=output_folder, filename=f"{filename}mp4")
 
-            data["songs"][data["n_songs"]] = song
+        mp4_file = os.path.join(output_folder, f"{filename}mp4")
+        wav_file = os.path.join(output_folder, f"{filename}wav")
 
-            options={
-                "format":"bestaudio/best",
-                "keepvideo":False,
-                "outtmpl":filename,
-                "postprocessor":[{
-                    "key":"FFmpegExtractAudio",
-                    "preferreddcodec":"wav"
-                }]
-            }
-            try:
-                with youtube_dl.YoutubeDL(options) as ydl:
-                    ydl.download([video_info['webpage_url']])
-                    # stream = ffmpeg.input(f"{data['n_songs']}.m4a")
-                    # stream = ffmpeg.output(stream, f"{data['n_songs']}.wav")
-            except youtube_dl.utils.DownloadError as e:
-                print(f"Error: {e}")
-                if "Sign in to confirm your age" in str(e):
-                    return False
-                print("Try again:")
-                continue
-            except youtube_dl.utils.ExtractorError as e:
-                print(f"Error: {e}")
-                if "Sign in to confirm your age" in str(e):
-                    return False
-                print("Try again:")
-                continue
+        ffmpeg.input(mp4_file).output(wav_file, ar=44000, ac=1).run(overwrite_output=True)
+        os.remove(mp4_file)
 
-            sleep(1)
-            os.chdir("music")
-            os.system(f"ffmpeg -i {data['n_songs']}.m4a -ar 44000 {data['n_songs']}.wav")
-            os.system(f"ffmpeg -i {data['n_songs']}.webm -ar 44000 {data['n_songs']}.wav")
-            os.system(f"del {data['n_songs']}.m4a")
-            os.system(f"del {data['n_songs']}.webm")
-            os.chdir("..")
-
-            print(f"Download complete... {filename}")
-
-            data["n_songs"] += 1
-
-            with open("music.json", "w") as f:
-                json.dump(data, f, indent=4)
-            break
-
+        return True
 
 if __name__ == '__main__':
-    os.chdir("music")
-    os.system(f"del *.part")
-    os.chdir("..")
     ar = AudioExtractor()
     with open("links.json") as f:
         data = json.load(f)
     
     for link in deepcopy(data["front"]):
-        if ar.extract(link) != False:
+        if ar.extract(link, len(data["done"])):
             data["done"].append(link)
-        data["front"].pop()
+        data["front"].pop(0)
         with open("links.json", "w") as f:
             json.dump(data, f, indent=4)
 
